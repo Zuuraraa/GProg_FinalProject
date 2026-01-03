d90968170f1b01c4af14be06ec07bc5404a89125
Azura <feliciaangel499@gmail.com>
2026-01-03 19:27:44 +0700

    game-over-ui

---
+++ Assets/Script/UI/GameOverManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public static GameOverManager instance;
+
+    [Header("UI Components")]
+    public GameObject gameOverPanel;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+    public void TriggerGameOver()
+    {
+        gameOverPanel.SetActive(true);
+        Time.timeScale = 0f;
+        Debug.Log("Game Over!");
+    }
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1f;
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene("MainMenu"); 
+    }
+}
